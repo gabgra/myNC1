2fa241ae809a1865b0a03807698c0ff30fef278a
Gabriella Gracia <gabriellagraciamt@icloud.com>
2019-05-20 12:12:37 +0700

    Initial Commit

---
+++ myNC1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myNC1
+//
+//  Created by Gabriella Gracia MT on 20/05/19.
+//  Copyright © 2019 Gabriella Gracia MT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
